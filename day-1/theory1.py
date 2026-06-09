5ab894b1ca48955f56e79ce998442caec201da8a
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-09 21:59:44 +0530

    Update theory1.py

--- day-1/theory1.py
+++ day-1/theory1.py
@@ -1,2 +1,18 @@
 "# My theory notes" 
+python was introduced by "GUDIO VAN ROSSUM"
+why python?
+    Interpreted language.
+    It is high level programming language.
+     Easy to learn.
+     Easy to test and deploy.
+      
+ what is compiler?
+      It  converts high level language into machine code.
+ what is interpreter?
+      execute line-by-line.
+  *variables are used to store the vaiues.
+      a=10
+      name="pavi"
 
+      
+    
