5b39835da488fe03366f7958eaeca4111addae19
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-10 10:38:36 +0530

    Update day1.py

--- day-1/day1.py
+++ day-1/day1.py
@@ -2,3 +2,19 @@ name="pavi"
 age=20
 print("name:",name,"age:",age)
 print("hello")
+# text output
+print("Hello, World!")
+# numbers
+print(123)
+print(3.14)
+print("Day", 1, "Practice")
+name = "Pavithra"
+day = 1
+print("My name is", name, "and this is Day", day)
+# Printing math results
+print("2 + 3 =", 2 + 3)
+print("10 / 2 =", 10 / 2)
+# lists
+fruits = ["apple", "banana", "cherry"]
+print("Fruits list:", fruits)
+
