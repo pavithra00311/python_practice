7b5ab09756905cfe58b88281b81313ab6fb6722a
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-10 10:59:43 +0530

    Update day1.py

--- day-1/day1.py
+++ day-1/day1.py
@@ -22,6 +22,50 @@ print(True)
 print(False)
 student = {"name": "Anush", "age": 20, "course": "Python"}
 print("Student info:", student)
+print("python"+ " " + "practice")
+# Output: Python Practice
+# Printing boolean values
+print(True)        # Output: True
+print(False)       # Output: False
+
+# String concatenation
+print("Python" + " " + "Practice")  
+# Output: Python Practice
+
+# Repeated strings
+print("Hello! " * 3)  
+# Output: Hello! Hello! Hello! 
+
+# Dictionary
+student = {"name": "Anush", "age": 20, "course": "Python"}
+print("Student info:", student)  
+# Output: Student info: {'name': 'Anush', 'age': 20, 'course': 'Python'}
+
+# Using format()
+print("My name is {} and I am {} years old.".format(student["name"], student["age"]))  
+# Output: My name is Anush and I am 20 years old.
+
+# Alignment
+print("{:<10} {:>10}".format("Left", "Right"))  
+# Output: Left             Right
+print("{:^20}".format("Centered"))  
+# Output:       Centered       
+
+# Separator
+print("apple", "banana", "cherry", sep=" | ")  
+# Output: apple | banana | cherry
+
+# End parameter
+print("This is line 1", end=" ... ")
+print("and continues on the same line")  
+# Output: This is line 1 ... and continues on the same line
+
+# Special characters
+print("Quotes: \"Double\" and 'Single'")  
+# Output: Quotes: "Double" and 'Single'
+print("Backslash: \\ Example")  
+# Output: Backslash: \ Example
+
 
 
 
