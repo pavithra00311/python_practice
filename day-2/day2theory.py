c2ee32c165c27c6c3934eac550143c447533d6f5
pavithra <pavithraeraganaboyina@gmail.com>
2026-06-10 16:13:01 +0530

    Update day2theory.py

--- day-2/day2theory.py
+++ day-2/day2theory.py
@@ -1,3 +1,95 @@
+OPERATORS
+Python operators are special symbols used to perform operations on variables and values. 
+They fall into the following main categories:
+
+• Arithmetic operators                 : +,-,*,/,%
+• Comparison (relational) operators    :=,<=,>=,==,!=
+• Logical operators                    : AND, OR, NOT
+• Bitwise operators		        : &,|,^,~,<<,>>
+• Assignment operators			: =,+=,-=,*=,%=
+• Identity operators			: is, is not
+• Membership operators                 : in, not in
+
+
+
+
+Arithmetic operators hands-on
+
+
+x=20
+y=10
+print("addition:", x+y)
+print("subtraction:", x-y)
+print("multiplication:", x*y)
+print("division:", x/y)
+print("modulus:", x % y)
+print("exponentiation:", x**y)
+print("floor division:", x//y)
+
+
+
+
+comparison operators:
+
+a=12
+b=30
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+
+
+logical operators
+
+a=False
+b=True
+
+print(a or b)
+print( a and b)
+print( not a)
+
+truth table:
+AND operator:
+
+condition A        condition B          result
+True                 True               True
+True                 false              false
+false                true               false
+false                false              false
+
+
+
+OR operator
+condition A          condition B          result
+
+True                 true                     true
+true                 false                   true
+false                true                      true
+false                false                     false
+
+
+
+
+Not operator
+
+condition 
+true  ---> false
+false ---> true
+
+
+
+
+
+
+
+
+
+
+
+
 Start small. Ship something.
 addition: 19
 subtraction: 1
